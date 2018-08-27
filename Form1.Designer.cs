6df5c7a089dc3cacead0337b85c2e020c8e7d94c
Jhoel Perez <jhoel.jjpr@gmail.com>
2018-08-26 23:07:22 -0500

    Demo 2
    Scroll down menu for worksheet loading

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace AddValue
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Wrapper1 = new AddValue.UserControl1();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.panel1.SuspendLayout();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Orange;
+			this.panel1.Controls.Add(this.comboBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(184, 116);
+			this.panel1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 54);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(66, 13);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Sheet Name";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(32, 18);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(116, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Choose Model";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel3.Controls.Add(this.dataGridView1);
+			this.panel3.Controls.Add(this.Wrapper1);
+			this.panel3.Location = new System.Drawing.Point(202, 12);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(596, 303);
+			this.panel3.TabIndex = 2;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(590, 356);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// Wrapper1
+			// 
+			this.Wrapper1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Wrapper1.Location = new System.Drawing.Point(0, 0);
+			this.Wrapper1.Name = "Wrapper1";
+			this.Wrapper1.Size = new System.Drawing.Size(596, 303);
+			this.Wrapper1.TabIndex = 0;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "addvalue_excelfile.xlsm";
+			this.openFileDialog1.Filter = "Excel files(*.xlsm)|*.*";
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.OrangeRed;
+			this.panel2.Controls.Add(this.button2);
+			this.panel2.Location = new System.Drawing.Point(12, 149);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(184, 71);
+			this.panel2.TabIndex = 4;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(55, 22);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "What\'s Best";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = false;
+			this.comboBox1.Location = new System.Drawing.Point(27, 70);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 2;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(801, 383);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Embedded Excel AddValue";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.panel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private UserControl1 Wrapper1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddValue
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private ArrayList Sheet_list = new ArrayList();
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Console.WriteLine("START Open File\n");
+
+			if (this.openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+				//filename
+				//sheet name
+				//drop down list
+				this.Wrapper1.File_open(this.openFileDialog1.FileName, this.dataGridView1, "",this.comboBox1);
+			}
+			Console.WriteLine("END Open File\n");
+			return;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Console.WriteLine("START Whats Best! \n");
+			try {
+				if (this.openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+				this.Wrapper1.File_open_web(this.openFileDialog1.FileName);
+			}
+
+			catch {
+				Console.WriteLine("Whats best button failed");
+			}
+			Console.WriteLine("END Whats Best! \n");
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			//this.comboBox1.Items.Add(tmp);
+			var selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+			this.Wrapper1.File_open(this.openFileDialog1.FileName, this.dataGridView1, selected, this.comboBox1);
+		}
+	}
+}
